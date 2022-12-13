a14699ee5124b52f1d668de6367e35c601ab6be6
RobertDoms <robertdoms90@gmail.com>
2022-12-13 22:02:09 +0200

    Create TransactionHistory.jsx

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { TransactionTable, Th, Td, Tbody } from './TransactionHistory.styled';
+const TransactionHistory = ({ items }) => {
+  return (
+    <TransactionTable>
+      <thead>
+        <tr>
+          <Th>Type</Th>
+          <Th>Amount</Th>
+          <Th>Currency</Th>
+        </tr>
+      </thead>
+
+      <Tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <Td>{item.type}</Td>
+            <Td>{item.amount}</Td>
+            <Td>{item.currency}</Td>
+          </tr>
+        ))}
+      </Tbody>
+    </TransactionTable>
+  );
+};
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default TransactionHistory;
