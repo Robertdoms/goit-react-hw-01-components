aaef1ea4b2e5e4393c6bb3c93258c9a888d446b5
RobertDoms <robertdoms90@gmail.com>
2022-12-17 22:33:11 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,4 +1,27 @@
-// import PropTypes from 'prop-types';
-// import { }
+import PropTypes from 'prop-types';
+import {
+  Container,
+  FriendCard,
+  FriendName,
+  FriendAvatar,
+  FriendOnlineStatus,
+} from 'components/FriendList/FriendList.styled';
 
-// export default FriendList;
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
+
+export default FriendList;
+
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>;
