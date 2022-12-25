6741c313dc0fff5a390a61a4e4bec52f26504fa5
RobertDoms <robertdoms90@gmail.com>
2022-12-25 09:56:08 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,12 +5,28 @@ import FriendListItem from 'components/FriendList/FriendListItem';
 const FriendList = ({ friends }) => {
   return (
     <Container>
-      <FriendListItem friends={friends} />
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendListItem
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            key={id}
+          />
+        );
+      })}
     </Container>
   );
 };
 
 FriendList.propTypes = {
-  friends: PropTypes.string.isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
 };
 export default FriendList;
